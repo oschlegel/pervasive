bd6c656f8ed72d61a24a5b8f603782a0db92fa5a
oschlegel <oli.schlegel@web.de>
2015-04-03 13:43:01 +0200

    Beacons erkennen und in listView anzeigen

--- MyApplication/app/src/main/java/com/computing/pervasive/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/computing/pervasive/myapplication/MainActivity.java
@@ -2,21 +2,28 @@ package com.computing.pervasive.myapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.MonitorNotifier;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements BeaconConsumer{
 
     private ListView mainListView;
     private ArrayAdapter<String> mainListAdapter;
-    private BeaconManager beaconmanager;
+    private BeaconManager beaconManager;
+    private Map<String,String> roomIdetifiers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +31,65 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mainListView = (ListView) findViewById( R.id.mainListView );
-
-        String[] rooms = new String[] {"Raum 2/101","Raum 3/101","Raum 4/101"};
-
-        mainListAdapter = new ArrayAdapter<String>(this, R.layout.simple_row,rooms);
+        mainListAdapter = new ArrayAdapter<String>(this, R.layout.simple_row,new String[]{});
         mainListView.setAdapter(mainListAdapter);
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+
+                // Get roomname for beaconid from database
+                String rangeID = region.getUniqueId();
+                String roomname;
+
+                roomname = "Roomname";// <<< Get roomname from database <<<
+
+                if (roomname!="") {
+                    roomIdetifiers.put(rangeID, roomname);
+
+                    // Add roomname for listView
+                    ArrayAdapter<String> adapter = (ArrayAdapter<String>) mainListView.getAdapter();
+                    adapter.add(roomIdetifiers.get(rangeID));
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+
+                String rangeID = region.getUniqueId();
+
+                if (roomIdetifiers.containsKey(rangeID)) {
+
+                    //Remove roomname from listView
+                    ArrayAdapter<String> adapter = (ArrayAdapter<String>) mainListView.getAdapter();
+                    adapter.remove(roomIdetifiers.get(rangeID));
+                    adapter.notifyDataSetChanged();
+
+                    roomIdetifiers.remove(rangeID);
+                }
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int state, Region region) {
+
+            }
+        });
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
